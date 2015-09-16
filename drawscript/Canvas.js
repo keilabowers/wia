6748692d6b69a4d2b15d45c2f9cbaaf8b227e72e
bowersk <kdbowers@fullsail.edu>
2015-09-15 21:26:39 -0400

    Drawscript practice
    Need to find something similar to DS or revert back a few illustrator
    vs.

---
+++ drawscript/Canvas.js
@@ -0,0 +1,19 @@
+window.onload = function(){
+	var the Canvas = document.getElementById("Canvas1");
+	if(theCanvas && theCanvas.getContext){
+		var ctx = theCanvas.getContext("2d");
+		if (ctx){
+			//Begin Drawing Here
+
+			//Drawscript doesnt work for the version of illustrator I have 
+
+			/* In Illustrator create your shape, select library (JS Canvas, etc), then select your prefix (ctx.) highlight what you want the code for and then hit 'generate'
+			This can also work with "font awesome", add a glyph. Make sure to 'create outline', then ungroup the items, then release compound paths
+			*/
+
+			/* works with createjs too.
+			select library 'createjs tiny', then add prefix
+			*/
+		}
+	}
+}
